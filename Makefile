b800a41e1b9ed8362487d1d9a5968e4d6dd578bf
Mohamed fadel Labrayji <mlabrayj@e2r4p12.1337.ma>
2021-02-02 11:47:03 +0100

    2main

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mlabrayj <mlabrayj@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/26 14:58:53 by mlabrayj          #+#    #+#              #
-#    Updated: 2021/02/01 12:15:17 by mlabrayj         ###   ########.fr        #
+#    Updated: 2021/02/01 17:41:28 by mlabrayj         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,6 +14,7 @@ ASM = nasm -f macho64
 SRC = ft_strlen.s ft_strcpy.s ft_strcmp.s ft_write.s ft_read.s ft_strdup.s
 O = $(SRC:.s=.o)
 NAME = libasm.a
+FLAG = -L. -lasm
 
 all: $(NAME)
 
@@ -25,6 +26,9 @@ $(NAME): $(O)
 
 clean:
 		rm -f $(O)
+
+test:
+		gcc -Wall -Wextra -Werror $(FLAG) $(NAME) main.c
 
 fclean: clean
 		rm -f $(NAME) a.out
--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/errno.h>
 
 size_t	ft_strlen(const char *s);
 char	*ft_strcpy(char *dst, const char *src);
 int		ft_strcmp(const char *s1, const char *s2);
-ssize_t	ft_write();
-ssize_t ft_read();
+ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t ft_read(int filedes, const void *buf, size_t nbyte);
+char *ft_strdup(const char *s1);
 
 
 
@@ -16,6 +18,18 @@ int main()
 	// const char *s1,*s2;
 	// s1 = "holaco";
 	// int ret = strcmp(s1, s2);
-	printf("%ld\n", ft_write());
+	// printf("%ld\n", ft_write());
+	// ft_write(-1, NULL, 10);
+	// perror("wirte");
+	// printf("The error message is: %d\n", errno);
+	// const char *s1 = "bc";
+	// const char *s2 = "az";
+	// ft_write(1, "hello", 3);
+	// const void *fd;
+	// ft_read(1, fd, 2);
+	// printf("%s", fd);
+	char *s1 ;
+	char *rs = ft_strdup(s1);
+	printf("%s\n", rs);
 	return (0);
 }
